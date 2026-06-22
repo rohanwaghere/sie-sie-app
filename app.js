a5e8b5a5af0e8f6febd6e85685b9a3972212253a
Rohan Waghere <rohanwaghere@gmail.com>
2026-06-22 18:21:13 -0400

    Update app.js

--- app.js
+++ app.js
@@ -286,10 +286,10 @@ function checkAnswer(id, selectedIndex) {
 
 function renderExamSelect() {
   const container = document.getElementById("exam-select");
-  container.innerHTML = 
-     `<button onclick = "startRandomExam()">Random Exam</button>` +
+  container.innerHTML =
+      `<button onclick="startRandomExam()">Random Exam</button>` +
     EXAMS.map(exam => 
-     `<button onclick = "setExam(${exam.id})">${exam.name}</button>`
+     `<button onclick="setExam(${exam.id})">${exam.name}</button>`
     ).join("");
 }
 
