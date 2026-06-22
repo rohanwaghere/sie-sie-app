a071684f85f2ab979e780f2cfd30581d40ba36d2
Rohan Waghere <rohanwaghere@gmail.com>
2026-06-22 11:44:25 -0400

    Create exams.js

---
+++ data/exams.js
@@ -0,0 +1,19 @@
+// data/exams.js
+
+const EXAMS = [
+  {
+    id: 1,
+    name: "SIE Practice Exam 1",
+    questionIds: [1, 2, 101, 201, 301] // expand to 75 IDs
+  },
+  {
+    id: 2,
+    name: "SIE Practice Exam 2",
+    questionIds: [2, 101, 201, 301] // expand to 75 IDs
+  },
+  {
+    id: 3,
+    name: "SIE Practice Exam 3",
+    questionIds: [1, 201, 301] // expand to 75 IDs
+  }
+];
